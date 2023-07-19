ea1c8def5cb3bfb49a344025e6ec6e3ec37afcab
Gustavo <119255970+GustavoSRodriguess@users.noreply.github.com>
2023-07-19 18:56:06 -0300

    Update Config.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -4,15 +4,7 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDi9Loa9b6fLKv_XJT-JaTmDH77npWJLnE",
-    authDomain: "ecomerce-62638.firebaseapp.com",
-    projectId: "ecomerce-62638",
-    storageBucket: "ecomerce-62638.appspot.com",
-    messagingSenderId: "473280378574",
-    appId: "1:473280378574:web:5baccc4557b74df4cf429f",
-    measurementId: "G-23KNK3R3NJ"
-  };
+//Youer firebase config 
 
   firebase.initializeApp(firebaseConfig)
 
